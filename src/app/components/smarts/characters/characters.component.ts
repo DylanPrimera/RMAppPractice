1ec38f8a568492b70482b08fa5c80db9fa38197c
DylanPrimera <dylanprimera98@gmail.com>
2021-07-25 14:04:37 -0500

    scrool to top btn

--- src/app/components/smarts/characters/characters.component.ts
+++ src/app/components/smarts/characters/characters.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, HostListener, Inject, OnDestroy, OnInit} from '@angular/core';
 import {RmApiService} from '../../../services/rm-api.service';
 import {Observable, Subscription} from 'rxjs';
 import {CharacterType} from '../../../util/types/characters/character-type';
-import {LocalStorageService} from '../../../services/local-storage.service';
+import {DOCUMENT} from '@angular/common';
 
 @Component({
   selector: 'app-characters',
@@ -12,12 +12,24 @@ import {LocalStorageService} from '../../../services/local-storage.service';
 export class CharactersComponent implements OnInit, OnDestroy {
   public characters$: Observable<CharacterType[]>;
   public subscription: Subscription = new Subscription();
+  public showBtn = false;
 
-  constructor(private rmService: RmApiService, private localStorageService: LocalStorageService) { }
+  constructor( @Inject(DOCUMENT) private document: Document, private rmService: RmApiService) { }
 
   ngOnInit(): void {
     this.subscription.add(this.rmService.getData().subscribe());
     this.characters$ = this.rmService.characters$;
+  }
+
+  @HostListener('window:scroll')
+  onScroll(): void {
+    const yOffSet = window.pageYOffset;
+    const scroll = this.document.documentElement.scrollTop;
+    this.showBtn = (yOffSet || scroll) > 500;
+  }
+
+  scrollTop(): void {
+    this.document.documentElement.scrollTop = 0;
   }
 
   ngOnDestroy(): void {
